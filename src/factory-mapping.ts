a01db237f793cc3790b1d04a9d4658b6099a85b4
Cristopher Ortega <ortega.cpp@gmail.com>
2021-02-17 00:30:13 -0400

    Add V2 Registration

--- src/factory-mapping.ts
+++ src/factory-mapping.ts
@@ -1,5 +1,4 @@
 import { BigInt, Address, log } from "@graphprotocol/graph-ts";
-import { Register as RegisterV1 } from "../generated/V1Factory/V1Factory";
 import {
   Register as RegisterV2,
   Delete,
@@ -9,6 +8,7 @@ import {
   Register as RegisterV21,
 } from "../generated/V21Factory/V21Factory";
 import {
+  MolochV2Template,
   MolochV21Template,
 } from "../generated/templates";
 
@@ -74,6 +74,78 @@ export function handleSummonV21(event: SummonComplete): void {
   moloch.totalShares = mTotalShares;
 
   moloch.save();
+}
+
+export function handleRegisterV2(event: RegisterV2): void {
+  MolochV2Template.create(event.params.moloch);
+
+  let molochId = event.params.moloch.toHex();
+  let moloch = new Moloch(molochId);
+  let daoMeta = new DaoMeta(event.params.moloch.toHex());
+  daoMeta.title = event.params.title;
+  daoMeta.version = "2";
+  daoMeta.newContract = "1";
+  daoMeta.save();
+
+  let tokens = event.params.tokens;
+  let approvedTokens: string[] = [];
+
+  for (let i = 0; i < tokens.length; i++) {
+    let token = tokens[i];
+    approvedTokens.push(createAndApproveToken(molochId, token));
+    createEscrowTokenBalance(molochId, token);
+    createGuildTokenBalance(molochId, token);
+  }
+
+  moloch.summoner = event.params.summoner;
+  moloch.summoningTime = event.params._summoningTime;
+  moloch.createdAt = event.params._summoningTime.toString();
+  moloch.title = event.params.title;
+  moloch.version = "2";
+  moloch.deleted = false;
+  moloch.newContract = "1";
+  moloch.periodDuration = event.params._periodDuration;
+  moloch.votingPeriodLength = event.params._votingPeriodLength;
+  moloch.gracePeriodLength = event.params._gracePeriodLength;
+  moloch.proposalDeposit = event.params._proposalDeposit;
+  moloch.dilutionBound = event.params._dilutionBound;
+  moloch.processingReward = event.params._processingReward;
+  moloch.depositToken = approvedTokens[0];
+  moloch.approvedTokens = approvedTokens;
+  moloch.totalShares = BigInt.fromI32(1);
+  moloch.totalLoot = BigInt.fromI32(0);
+
+  moloch.save();
+
+  let memberId = molochId
+    .concat("-member-")
+    .concat(event.params.summoner.toHex());
+  let newMember = new Member(memberId);
+  newMember.moloch = molochId;
+  newMember.molochAddress = event.params.moloch;
+  newMember.memberAddress = event.params.summoner;
+  newMember.createdAt = event.block.timestamp.toString();
+  newMember.delegateKey = event.params.summoner;
+  newMember.shares = BigInt.fromI32(1);
+  newMember.loot = BigInt.fromI32(0);
+  newMember.exists = true;
+  newMember.tokenTribute = BigInt.fromI32(0);
+  newMember.didRagequit = false;
+  newMember.proposedToKick = false;
+  newMember.kicked = false;
+
+  newMember.save();
+
+  for (let i = 0; i < tokens.length; i++) {
+    let token = tokens[i];
+    let tokenId = molochId.concat("-token-").concat(token.toHex());
+    createMemberTokenBalance(
+      molochId,
+      event.params.summoner,
+      tokenId,
+      BigInt.fromI32(0)
+    );
+  }
 }
 
 export function handleRegisterV21(event: RegisterV21): void {
